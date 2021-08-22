73c3240b18cf7a13130a06339b25d89b31c43282
mariagrosu <grosu.mari@mail.ru>
2021-08-22 12:12:37 +0300

    Initial Commit

---
+++ TableViewSettingsMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewSettingsMVC
+//
+//  Created by Маша on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
